d06d4450331258d149351464d102178416e29a50
Guilherme Bonifácio <109730367+Goneleven@users.noreply.github.com>
2025-03-28 19:17:12 -0300

    Finalizado 2.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # InfoSite
-Site que eu recriei do zero o site de Undertale e Deltarune só que no meu estilo
+Site mostrando informação de Dois assuntos totalmente diferentes
--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
+// js para mudar o modo do site
 document.querySelector('.botao-modo').addEventListener('click', (e) => {
-    e.target.classList.toggle('selecao-move');
+    e.target.classList.toggle('selecao-fundo-imagem');
     document.body.classList.toggle('light');
 
 });
